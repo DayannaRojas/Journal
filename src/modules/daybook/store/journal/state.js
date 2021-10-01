9f2bf78f4b8f2e54d740a378a3728ad50657c4b6
Dayanna Rojas Miranda <drojas@factoacr.com>
2021-10-01 11:15:57 -0600

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,27 @@
+/* export default () =>({
+
+} ) */
+
+export default () =>({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Aliquip qui amet aliqua labore minim ea ut irure. Mollit nisi incididunt officia esse commodo sunt ad aliqua ea commodo officia commodo irure. Mollit et Lorem occaecat Lorem tempor aliquip exercitation. Officia proident non tempor voluptate consectetur. Incididunt occaecat Lorem excepteur sint consequat adipisicing ut id do eu voluptate. Nostrud elit aliquip nulla occaecat dolore amet dolor est aliquip consectetur do Lorem officia. Magna in ipsum fugiat in in minim occaecat in non fugiat ipsum veniam reprehenderit incididunt.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Dayanna Rojas Miranda',
+            picture: null
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Selena Mairena Miranda',
+            picture: null
+        },
+    ]
+} ) 
